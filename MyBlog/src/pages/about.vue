cc4d372458517a31d4accf7560edcc5fced6f91d
Yanusov Andrey <jdkz@ya.ru>
2019-02-06 21:43:55 +0700

    e

---
+++ MyBlog/src/pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex ">
+    <h1>Андрей Янусов</h1>
+    <p>О странице!</p>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
---
+++ MyBlog/src/router/routes.js
@@ -0,0 +1,23 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: 'about', component: () => import('pages/about.vue') },
+      { path: 'link', component: () => import('pages/link.vue') }
+
+    ]
+  }
+]
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
+}
+
+export default routes
